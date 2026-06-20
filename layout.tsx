6f57644760c33d1529dd8b084fb3afbf8e91c6b8
sl990322-tech <sl990322@gmail.com>
2026-06-20 19:42:52 +0800

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,17 @@
+import Sidebar from "./components/Sidebar";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+
+      <main className="flex-1 bg-gray-100 p-8">
+        {children}
+      </main>
+    </div>
+  );
+}
